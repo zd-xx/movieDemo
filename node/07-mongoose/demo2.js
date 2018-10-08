5c386590a97acdb1482abf2292df376fbf9a7db7
EGOD <1037597461@qq.com>
2018-10-08 10:48:13 +0800

    mongoose

--- node/07-mongoose/demo2.js
+++ node/07-mongoose/demo2.js
@@ -25,17 +25,71 @@ var userSchema = new Schema({
 //返回：模型构造函数，有此函数可以对user中数据操作
 const User = mongoose.model('User', userSchema);
 
-//增加数据
-var admin = new User({
-	username:'admin',
-	password:'123456',
-	email:'admin@admin.com'
-})
-//保存数据，ret为插入的这个数据
-admin.save(function(err,ret) {
+//增加数据------------------------
+// var admin = new User({
+// 	username:'admin',
+// 	password:'123456',
+// 	email:'admin@admin.com'
+// })
+
+// admin.save(function(err,ret) {//保存数据，ret为插入的这个数据
+// 	if(err){
+// 		console.log('保存失败！')
+// 	}else{
+// 		console.log('保存成功！')
+// 	}
+// })
+//查看数据------------------------------
+//查询所有：
+
+User.find(function(err,ret){
 	if(err){
-		console.log('保存失败！')
+		console.log('查询失败！')
 	}else{
-		console.log('保存成功！')
+		console.log(ret)
 	}
 })
+
+//按条件查询所有（数组）：
+// User.find({
+// 	username:'admin'
+// },function(err,ret){
+// 	if(err){
+// 		console.log('查询失败！')
+// 	}else{
+// 		console.log(ret)
+// 	}
+// })
+ 
+//按照条件查找符合的第一个数据（对象），若无条件则查找第一个数据
+// User.findOne({
+// 	username:'admin'
+// },function(err,ret){
+// 	if(err){
+// 		console.log('查询失败！')
+// 	}else{
+// 		console.log(ret)
+// 	}
+// })
+
+//删除数据
+// User.remove({
+// 	username:'admin'
+// },function(err,ret){
+// 	if(err){
+// 		console.log("删除失败！")
+// 	}else{
+// 		console.log(ret)
+// 	}
+// })
+// 
+// 更新数据ret为更改前的数据
+// User.findByIdAndUpdate('5bba360c87d30d16d099d51a',{
+// 	password:'999'
+// },function(err,ret){
+// 	if(err){
+// 		console.log("更新成功！")
+// 	}else{
+// 		console.log(ret)
+// 	}
+// })
