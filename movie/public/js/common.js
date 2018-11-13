530b8e28e6a197391c9607f226aa33de8e65734d
zd-xx <1037597461@qq.com>
2018-11-13 21:52:04 +0800

    up

--- movie/public/js/common.js
+++ movie/public/js/common.js
@@ -10,6 +10,9 @@ window.onload = function(){
 		$header.on('click',function(){
 			$QRcode.removeClass('hide')
 		})
+		$('.mui-title').on('click',function(){
+			$QRcode.removeClass('hide')
+		})
 		$('.closeQR').on('click',function(){
 			$QRcode.addClass('hide')
 		})
@@ -17,21 +20,4 @@ window.onload = function(){
 			$QRcode.addClass('hide')
 		})
 	})()
-	//
-// 	!(function(){
-// 		var list = document.querySelector('#movie-list');
-// 		alert(list)
-// 		mui.init({
-//   	pullRefresh : {
-//     container:list,//待刷新区域标识，querySelector能定位的css选择器均可，比如：id、.class等
-//     up : {
-//       height:50,//可选.默认50.触发上拉加载拖动距离
-//       auto:true,//可选,默认false.自动上拉加载一次
-//       contentrefresh : "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
-//       contentnomore:'没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
-//       callback :null //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-//     }
-//   }
-// })
-// 	})()
 }
